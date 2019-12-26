bd1abb3e1ac614654ca3149837e8f0647c0dd35a
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-12-26 04:25:40 +0400

    added file input

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import 'bootstrap-select/dist/js/bootstrap-select.min';
 import 'stacktable.js/stacktable';
 import 'datatables-bootstrap/js/dataTables.bootstrap.min';
 //import 'blueimp-file-upload/js/jquery.fileupload';
+import 'bootstrap-fileinput/js/fileinput.min';
+
 import './sass/main.scss'
 
 import './script';
--- src/script.js
+++ src/script.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
-    console.log("Hello");
+    $("#fileInput").fileinput({
+        previewFileIcon: "<i class=\"fa fa-edit\"></i>",
+        previewFileType: "image",
+        browseClass: "btn btn-success",
+        browseLabel: "Pick Image",
+        browseIcon: "<i class=\"fa fa-edit\"></i> ",
+        removeClass: "btn btn-danger",
+        removeLabel: "Delete",
+        removeIcon: "<i class=\"fa fa-remove\"></i> ",
+        uploadClass: "btn btn-info",
+        uploadLabel: "Upload",
+        uploadIcon: "<i class=\"fa fa-edit\"></i> "
+    }); // {showCaption: false, dropZoneEnabled: false}
     $('.usersTable').stacktable({myClass:'responsive'});
 
     $('#usersTable').DataTable();
 
     //$('#fileupload').fileupload();
+
+
+
 });
