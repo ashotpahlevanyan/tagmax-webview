dc1737afa61da2b1cbc5865014b634743c0e4c3a
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-12-26 14:36:15 +0400

    fixed file input

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'bootstrap'
 import 'bootstrap-select/dist/js/bootstrap-select.min';
 import 'stacktable.js/stacktable';
 import 'datatables-bootstrap/js/dataTables.bootstrap.min';
-//import 'blueimp-file-upload/js/jquery.fileupload';
 import 'bootstrap-fileinput/js/fileinput.min';
 
 import './sass/main.scss'
--- src/script.js
+++ src/script.js
@@ -1,23 +1,48 @@
 $(document).ready(function(){
-    $("#fileInput").fileinput({
-        previewFileIcon: "<i class=\"fa fa-edit\"></i>",
-        previewFileType: "image",
-        browseClass: "btn btn-success",
-        browseLabel: "Pick Image",
-        browseIcon: "<i class=\"fa fa-edit\"></i> ",
-        removeClass: "btn btn-danger",
-        removeLabel: "Delete",
-        removeIcon: "<i class=\"fa fa-remove\"></i> ",
-        uploadClass: "btn btn-info",
-        uploadLabel: "Upload",
-        uploadIcon: "<i class=\"fa fa-edit\"></i> "
-    }); // {showCaption: false, dropZoneEnabled: false}
+    // $("#fileInput").fileinput({
+    //     previewFileIcon: "<i class=\"fa fa-edit\"></i>",
+    //     previewFileType: "image",
+    //     browseClass: "btn btn-success",
+    //     browseLabel: "Pick Image",
+    //     browseIcon: "<i class=\"fa fa-edit\"></i> ",
+    //     removeClass: "btn btn-danger",
+    //     removeLabel: "Delete",
+    //     removeIcon: "<i class=\"fa fa-remove\"></i> ",
+    //     uploadClass: "btn btn-info",
+    //     uploadLabel: "Upload",
+    //     uploadIcon: "<i class=\"fa fa-edit\"></i> "
+    // }); // {showCaption: false, dropZoneEnabled: false}
     $('.usersTable').stacktable({myClass:'responsive'});
 
     $('#usersTable').DataTable();
 
     //$('#fileupload').fileupload();
 
+    $("#fileInput").fileinput({
+        showPreview: true,
+        showCaption: true,
+        browseClass: "btn btn-success btn-sm withIcon browse",
+        browseLabel: "",
+        removeClass: "btn btn-danger btn-sm withIcon",
+        removeLabel: "Delete",
+        uploadClass: "btn btn-info btn-sm withIcon",
+        cancelClass: "btn btn-secondary btn-sm withIcon",
+        uploadLabel: "Upload",
+        removeIcon: "<i class=\"fa fa-trash\"></i> ",
+        uploadIcon: "<i class=\"fa fa-upload\"></i> ",
+        cancelIcon: "<i class=\"fa fa-close\"></i> ",
+        allowedPreviewTypes: ["text"],
+        allowedPreviewExtensions: ["csv"],
+        fileTypeSettings: {
+            office: function (vType, vName) {
+                return vType.match(/(word|excel|powerpoint|office)$/i) ||
+                    vName.match(/\.(docx?|xlsx?|pptx?|pps|potx?)$/i);
+            },
+            text: function(vType, vName) {
+                return typeof vType !== "undefined" && vType.match('text.*') || vName.match(/\.(txt|md|csv|nfo|php|ini)$/i);
+            }
+        }
+    });
 
 
 });
